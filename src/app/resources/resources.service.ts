4acb4bff35d923276a1bcb116d0eb872d782426f
Richard Sprince <sprince.richard@gmail.com>
2026-07-22 01:09:12 -0400

    Fixes URL for Resources

--- src/app/resources/resources.service.ts
+++ src/app/resources/resources.service.ts
@@ -6,7 +6,7 @@ import { map } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class ResourcesService {
-  url = '/data/content.json';
+  url = 'data/content.json';
   data: any;
 
   constructor(private http: HttpClient) {}
